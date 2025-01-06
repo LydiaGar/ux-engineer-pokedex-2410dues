1d2b4afdc24382c859a518ee0e7068da93519474
L <lydiagarciase@gmail.com>
2025-01-06 12:05:20 +0100

    add more pokemons

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,8 +1,23 @@
 const pokemonData = [
     {
         id: 1,
-        name: "Bulbasur",
+        name: "Bulbasaur",
         image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+    },
+    {
+        id: 4,
+        name: "Charmander",
+        image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+    },
+    {
+        id: 7,
+        name: "Squirtle",
+        image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+    },
+    {
+        id: 25,
+        name: "Pikachu",
+        image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
     },
 ];
 
